22012a1a7ad3fc44c72da53ff69bbb26db026ffe
Enes <ozdinenes@gmail.com>
2024-05-02 18:41:49 +0300

    added router and dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,76 +1,14 @@
-import React, {
-  ChangeEvent,
-  useState,
-  MouseEvent,
-  SyntheticEvent,
-} from "react";
+import React from "react";
 import "./App.css";
-import CardList from "./Components/CardList/CardList";
-import Search from "./Components/Search/Search";
-import { CompanySearch } from "./company";
-import { searchCompanies } from "./api";
-import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
+import { Outlet } from "react-router";
 import Navbar from "./Components/Navbar/Navbar";
-import Hero from "./Components/Hero/Hero";
 
 function App() {
-  const [search, setSearch] = useState<string>("");
-  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
-  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] = useState<string>("");
-
-  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    // console.log(e);
-  };
-  const onPortfolioCreate = (e: any) => {
-    e.preventDefault();
-    const exists = portfolioValues.find((value) => value === e.target[0].value);
-    if (exists) return;
-    const updatePortfolio = [...portfolioValues, e.target[0].value];
-    setPortfolioValues(updatePortfolio);
-  };
-
-  const onPortfolioDelete=(e:any)=>{
-    e.preventDefault();
-    const removed=portfolioValues.filter((value)=>{
-      console.log("filter value",value);
-      console.log("filter target value",e.target[0].value);
-      return value !== e.target[0].value;
-    });
-    console.log("after filter value",removed);
-    //removed=value degeri haric dizideki elemanlari icerir
-    setPortfolioValues(removed);
-  }
-
-  const onSearchSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    //type sorunu yasarsan SyntethicEvent kullanabilirsin.
-    const result = await searchCompanies(search);
-    if (typeof result === "string") {
-      setServerError(result);
-    } else if (Array.isArray(result.data)) {
-      setSearchResult(result.data);
-    }
-    console.log(searchResult);
-    console.log(e);
-  };
-
   return (
-    <div className="App">
-      <Navbar/>
-      <Search
-        onSearchSubmit={onSearchSubmit}
-        search={search}
-        handleSearchChange={handleSearchChange}
-      />
-      <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete}/>
-      <CardList
-        searchResults={searchResult}
-        onPortfolioCreate={onPortfolioCreate}
-      />
-      {serverError && <h1>Unable to connect to API</h1>}
-    </div>
+    <>
+      <Navbar />
+      <Outlet />
+    </>
   );
 }
 
---
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getCompanyProfile } from "../../api";
+import { CompanyProfile } from "../../company";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
+import Tile from "../../Components/Tile/Tile";
+
+interface Props { }
+
+const CompanyPage = (props: Props) => {
+  //https:localhost:3000/
+  let { ticker } = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+  useEffect(() => {
+    const getProfileInit = async () => {
+      const result = await getCompanyProfile(ticker!);
+      setCompany(result?.data[0]);
+    };
+    getProfileInit();
+  }, []);
+  return (
+    <>
+      {company ? (
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <Sidebar />
+          <CompanyDashboard><Tile title="Company Name" subTitle={company.companyName}></Tile>
+          </CompanyDashboard>
+
+        </div>
+      ) : (
+        <div>Company not found!</div>
+      )}
+    </>
+  );
+};
+export default CompanyPage;
